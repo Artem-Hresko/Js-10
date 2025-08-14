3753282a30c166a373d5793dd76e75ec5a90ea43
Artem Hresko <gresko.artem09@gmail.com>
2025-08-14 16:57:03 +0100

    z

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 // // TASK 1
 
-// const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
-// const words = ["кіт", "собака", "миша", "папуга", "хомяк"];
+const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
+const words = ["кіт", "собака", "миша", "папуга", "хомяк"];
 
-// // Функція countItems тут
+// Функція countItems тут
 
-// // Умови для перевірки:
-// const isEven = (num) => num % 2 === 0;
-// const isLarge = (num) => num > 10;
-// const isShort = (word) => word.length <= 3;
+// Умови для перевірки:
+const isEven = (num) => num % 2 === 0;
+const isLarge = (num) => num > 10;
+const isShort = (word) => word.length <= 3;
 
-// function countItems(array, condition) {
-//   let count = 0;
-//   for (const element of array) {
-//     if (condition(element)) {
-//       count++;
-//     }
-//   }
-//   return count;
-// }
+function countItems(array, condition) {
+  let count = 0;
+  for (const element of array) {
+    if (condition(element)) {
+      count++;
+    }
+  }
+  return count;
+}
 
-// console.log(countItems(numbers, isEven));
-// console.log(countItems(numbers, isLarge));
-// console.log(countItems(words, isShort));
+console.log(countItems(numbers, isEven));
+console.log(countItems(numbers, isLarge));
+console.log(countItems(words, isShort));
 
 // Task 2
 
@@ -38,8 +38,20 @@ const divide = (a, b) => {
   return a / b;
 };
 
-console.log(calculate(10, 5, add)); // Повинно показати 15
-console.log(calculate(10, 5, subtract)); // Повинно показати 5
-console.log(calculate(10, 5, multiply)); // Повинно показати 50
-console.log(calculate(10, 5, divide)); // Повинно показати 2
-console.log(calculate(10, 0, divide)); // Повинно показати помилку
+console.log(calculate(10, 5, add));
+console.log(calculate(10, 5, subtract));
+console.log(calculate(10, 5, multiply));
+console.log(calculate(10, 5, divide));
+console.log(calculate(10, 0, divide));
+
+// Task 3
+
+function repeatMessage(times, messageCreator) {
+  for (let i = 0; i < times; i++) {
+    messageCreator(i);
+  }
+}
+
+repeatMessage(5, (index) => {
+  console.log(`Повідомлення #${index + 1}`);
+});
